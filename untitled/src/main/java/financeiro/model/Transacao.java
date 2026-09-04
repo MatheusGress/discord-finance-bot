6d5a08dec608e8db9c979c05ae66701976ccc6e3
Matheus Gress <matheusaugustogress@gmail.com>
2026-09-04 06:42:29 -0300

    feat: add initial financial domain models

--- untitled/src/main/java/financeiro/model/Transacao.java
+++ untitled/src/main/java/financeiro/model/Transacao.java
@@ -1,4 +1,19 @@
 package financeiro.model;
 
 public class Transacao {
+    private UUID id;
+    private BigDecimal valor;
+    private TipoTransacao tipo;
+    private Categoria categoria;
+    private String descricao;
+    private LocalDate data;
+
+    public Transacao(UUID id, BigDecimal valor, TipoTransacao tipo, Categoria categoria, String descricao, LocalDate data){
+        this.id = id;
+        this.valor = valor;
+        this.tipo = tipo;
+        this.categoria = categoria;
+        this.descricao = descricao;
+        this.data = data;
+    }
 }
--- untitled/src/main/java/financeiro/model/Usuario.java
+++ untitled/src/main/java/financeiro/model/Usuario.java
@@ -1,4 +1,11 @@
 package financeiro.model;
 
 public class Usuario {
+    private UUID discordID;
+    private String nome;
+
+    private Usuario(UUIS discordID, String nome){
+        this.discordID = discordID;
+        this.nome = nome;
+    }
 }
