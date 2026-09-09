c39a500991f8a02c27187b08f8e4e52791092ac0
Matheus Gress <matheusaugustogress@gmail.com>
2026-09-09 07:22:11 -0300

    feat: implement in-memory transaction service

--- untitled/src/main/java/financeiro/model/Transacao.java
+++ untitled/src/main/java/financeiro/model/Transacao.java
@@ -1,5 +1,9 @@
 package financeiro.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class Transacao {
     private UUID id;
     private BigDecimal valor;
@@ -7,13 +11,38 @@ public class Transacao {
     private Categoria categoria;
     private String descricao;
     private LocalDate data;
+    private Cartao cartao;
 
-    public Transacao(UUID id, BigDecimal valor, TipoTransacao tipo, Categoria categoria, String descricao, LocalDate data){
+
+    public Transacao(UUID id, BigDecimal valor, TipoTransacao tipo, Categoria categoria, String descricao, LocalDate data, Cartao cartao){
         this.id = id;
         this.valor = valor;
         this.tipo = tipo;
         this.categoria = categoria;
         this.descricao = descricao;
         this.data = data;
+        this.cartao = cartao;
     }
+
+    public TipoTransacao getTipo(){
+        return tipo;
+    }
+
+    public BigDecimal getValor(){
+        return valor;
+    }
+
+    public LocalDate getData(){
+        return data;
+    }
+
+    public Cartao getCartao(){
+        return cartao;
+    }
+
+    public boolean isCartao(){
+        return cartao != null;
+    }
+
+
 }
